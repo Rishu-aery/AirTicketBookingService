563baaaea24412731d471e16f1cde2ccd6b72247
Rishabh-aery <rishab.arrey@quodd.com>
2026-02-03 22:29:59 +0530

    basic setup

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const v1ApiRoutes = require("./v1/v1ApiRoutes.js");
+
+router.use("/vi", v1ApiRoutes);
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const { PORT, DB_SYNC } = require("./config/serverConfig.js");
+const apiRoutes = require("./routes/index.js");
+const db = require("./models/index.js");
+
+const setupAndStartServer = () => {
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use('/api', apiRoutes);
+
+    app.listen(() => {
+        if (DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+        console.log(`Server Running At Port: ${PORT}`);
+    })
+}
+
+setupAndStartServer();
