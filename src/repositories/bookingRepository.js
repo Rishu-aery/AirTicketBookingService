7d8dc56abd25c9466158c8e0e62fe72a6362fd27
Rishabh-aery <rishab.arrey@quodd.com>
2026-02-05 23:27:21 +0530

    create booking

--- src/repositories/bookingRepository.js
+++ src/repositories/bookingRepository.js
@@ -4,14 +4,25 @@ const { Booking } = require("../models/index.js");
 class BookingRepository {
     async create(data) {
         try {
-            const result = Booking.create(data);
+            const result = await Booking.create(data);
             return result;
         } catch (error) {
             throw new ValidationError(error);
         }
     }
+
+    async update(bookingId, data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if (data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
-module.exports = {
-    BookingRepository
-}
+module.exports = BookingRepository
--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,16 +1,36 @@
 const { FLIGHT_SERVICE_URL } = require("../config/serverConfig.js");
+const { BookingRepository } = require("../repositories/index.js");
 const { InternalError } = require("../utils/error-handler.js");
 const axios = require("axios");
 
 class BookingService {
+    constructor() {
+        this.bookingRepository = new BookingRepository()
+    }
     async createBooking(data) {
         try {
             const flightId = data.flightId;
             const getFlightUrl = `${FLIGHT_SERVICE_URL}/api/v1/flight/${flightId}`;
-            const flight = await axios.get(getFlightUrl);
-            return flight.data.data;
+            const flightDetail = (await axios.get(getFlightUrl)).data.data;
+            if (!flightDetail) {
+                throw new InternalError();
+            }
+            if (data.bookedSeats > flightDetail.availableSeats) {
+                throw new InternalError("Insufficient seats available to complete your booking.");
+            }
+            const totalCost = flightDetail.price * data.bookedSeats;
+            const bookingPayload = {
+                ...data,
+                totalCost
+            }
+            const booking = await this.bookingRepository.create(bookingPayload);
+            const updateFlightUrl = `${FLIGHT_SERVICE_URL}/api/v1/flight/${flightId}`;
+            await axios.patch(updateFlightUrl, {availableSeats: flightDetail.availableSeats - booking.bookedSeats});
+            const finalBooking = this.bookingRepository.update(booking.id, {status: "Booked"});
+            return finalBooking;
         } catch (error) {
-            throw new InternalError("Internal Server Error!", error);   
+            console.log("server error--------------", error);
+            throw error;   
         }
     }
 }
